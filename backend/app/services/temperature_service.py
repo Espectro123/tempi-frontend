b1875783360cfb9c0818d73edde55a394b0160c2
Enrique <kicalin@gmail.com>
2023-12-19 02:23:36 +0100

    Fix the code a bit

--- backend/app/services/temperature_service.py
+++ backend/app/services/temperature_service.py
@@ -1,12 +1,10 @@
 from app.domain.entities.temperature_sensor import TemperatureSensor
 from app.repositories.in_memory_repository import InMemoryRepository
 from app.utils.read_temperature import read_temperature
-from random import randint
 from datetime import datetime
 
 class TemperatureService:
 
-    # Here I should add the way to read data from sensors. Must use sensor id
     @staticmethod
     def add_temperature_reading(sensor: TemperatureSensor, sensor_id):
         sensor.timestamp = datetime.now().isoformat()
